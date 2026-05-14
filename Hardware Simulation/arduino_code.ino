d5189548f1e285a015b85f67b3ee466fbe744828
Eddy <guptaeddy50@gmail.com>
2026-05-14 08:35:04 +0530

    Arduino code updated added Remaining Range instead of Throttle input on the LCD Dispaly

--- Hardware Simulation/arduino_code.ino
+++ Hardware Simulation/arduino_code.ino
@@ -1,12 +1,26 @@
 #include <LiquidCrystal.h>
 
+// LCD Pins
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
+// Pin Definitions
 int potPin = A0;
 int motorEnable = 9;
 int motorInput1 = 8;
 
+// Variables
+int throttle;
+int speedValue;
+
+float speed;
+float current;
+float voltage = 24.0;
+float power;
+float batteryCapacity = 240.0;
+float estimatedRange;
+
 void setup() {
+
   pinMode(motorEnable, OUTPUT);
   pinMode(motorInput1, OUTPUT);
 
@@ -17,28 +31,46 @@ void setup() {
 
 void loop() {
 
-  int throttle = analogRead(potPin);
+  // Read Potentiometer
+  throttle = analogRead(potPin);
 
-  int speedValue = map(throttle, 0, 1023, 0, 255);
+  // Map Motor Speed
+  speedValue = map(throttle, 0, 1023, 0, 255);
 
+  // Run Motor
   analogWrite(motorEnable, speedValue);
 
+  // Simulated Parameters
+  speed = map(throttle, 0, 1023, 0, 80);
+
+  current = map(throttle, 0, 1023, 1, 15);
+
+  // Power Calculation
+  power = voltage * current;
+
+  // Range Estimation
+  estimatedRange = (batteryCapacity / power) * speed;
+
+  // LCD Display
   lcd.clear();
 
-  lcd.setCursor(0,0);
-  lcd.print("Throttle:");
-  lcd.print(map(throttle,0,1023,0,100));
+  // First Line
+  lcd.setCursor(0, 0);
+  lcd.print("Range:");
+  lcd.print(estimatedRange, 1);
+  lcd.print("km");
 
-  lcd.setCursor(0,1);
+  // Second Line
+  lcd.setCursor(0, 1);
 
   if(throttle < 300)
     lcd.print("Mode: ECO");
 
   else if(throttle < 700)
-    lcd.print("Mode: NORMAL");
+    lcd.print("Mode:NORMAL");
 
   else
-    lcd.print("Mode: AGGRESS");
+    lcd.print("Mode:AGGRESS");
 
-  delay(300);
+  delay(500);
 }
