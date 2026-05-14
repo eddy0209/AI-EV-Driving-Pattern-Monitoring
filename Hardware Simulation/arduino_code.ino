8093d11b9d8ce2e7af10290cfe9f11583ab679f8
Eddy <guptaeddy50@gmail.com>
2026-05-14 08:36:39 +0530

    Arduino code updated added Remaining Range instead of Throttle input on the LCD Dispaly

--- Hardware Simulation/arduino_code.ino
+++ Hardware Simulation/arduino_code.ino
@@ -1,14 +1,14 @@
 #include <LiquidCrystal.h>
 
-// LCD Pins
+
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
-// Pin Definitions
+
 int potPin = A0;
 int motorEnable = 9;
 int motorInput1 = 8;
 
-// Variables
+
 int throttle;
 int speedValue;
 
@@ -31,36 +31,36 @@ void setup() {
 
 void loop() {
 
-  // Read Potentiometer
+
   throttle = analogRead(potPin);
 
-  // Map Motor Speed
+
   speedValue = map(throttle, 0, 1023, 0, 255);
 
-  // Run Motor
+
   analogWrite(motorEnable, speedValue);
 
-  // Simulated Parameters
+
   speed = map(throttle, 0, 1023, 0, 80);
 
   current = map(throttle, 0, 1023, 1, 15);
 
-  // Power Calculation
+
   power = voltage * current;
 
-  // Range Estimation
+
   estimatedRange = (batteryCapacity / power) * speed;
 
-  // LCD Display
+
   lcd.clear();
 
-  // First Line
+
   lcd.setCursor(0, 0);
   lcd.print("Range:");
   lcd.print(estimatedRange, 1);
   lcd.print("km");
 
-  // Second Line
+
   lcd.setCursor(0, 1);
 
   if(throttle < 300)
